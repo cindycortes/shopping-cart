a2f5dbba98213f166d5c59780df5cdd35410cdd5
Cindy Cortes <cindy.indins@gmail.com>
2019-01-30 13:40:52 -0700

    cartItemsList placed uner the render method

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import CartItems from './Components/CartItems';
 class App extends Component {
   render() {
 
+    cartItemsList = [
+      { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
+      { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
+      { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 }
+    ]
+
     return (
       <body>
         <Header />
